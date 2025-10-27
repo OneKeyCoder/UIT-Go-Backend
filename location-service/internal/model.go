0ca6aa74d82b1869c6e40c7ad913060e2a64aaad
BeCaBuCode <23520950@gm.uit.edu.vn>
2025-10-27 23:20:23 +0700

    Forgot to commit location model ehe

--- location-service/internal/model.go
+++ location-service/internal/model.go
@@ -2,6 +2,7 @@ package location_service
 
 type CurrentLocation struct {
 	UserID    int     `json:"user_id"`
+	Role      string  `json:"role"`
 	Latitude  float64 `json:"latitude"`
 	Longitude float64 `json:"longitude"`
 	Distance  float64 `json:"distance,omitempty"`
