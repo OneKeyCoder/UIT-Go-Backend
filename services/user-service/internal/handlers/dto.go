68489c3ec1d016900e4908c77d8b0ad47810d765
Minh dep trai <phandinhminh48@gmail.com>
2025-11-24 23:29:49 +0700

    add user service

--- services/user-service/internal/handlers/dto.go
+++ services/user-service/internal/handlers/dto.go
@@ -5,18 +5,3 @@ type ResponseDTO struct {
 	Message    string
 	Data       interface{}
 }
-
-type UserRequest struct {
-	Email        string `json:"email"`
-	Role         string `json:"role"`
-	DriverStatus string `json:"driver_status"`
-}
-
-type VehicleRequest struct {
-	VehicleId    int    `json:"vehicle_id"`
-	DriverId     int    `json:"driver_id"`
-	LicensePlate string `json:"license_plate"`
-	VehicleType  string `json:"vehicle_type"`
-	Seats        int    `json:"seats"`
-	Status       string `json:"status"`
-}
