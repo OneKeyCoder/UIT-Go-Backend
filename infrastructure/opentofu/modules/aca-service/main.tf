3afc890645b037e90268729ac50619a6e21badef
honguyenminh <honguyenminh@outlook.com>
2025-12-09 10:33:55 +0700

    make aca service module usable

--- infrastructure/opentofu/modules/aca-service/main.tf
+++ infrastructure/opentofu/modules/aca-service/main.tf
@@ -1,12 +1,28 @@
+locals {
+  image_name = var.image_name != null && var.image_name != "" ? var.image_name : var.name
+}
+
 resource "azurerm_container_app" "go-services" {
   name = var.name
   container_app_environment_id = var.container_app_environment_id
   resource_group_name = var.resource_group_name
   revision_mode = "Single"
 
-  secret {
-    name = ""
-    key_vault_secret_id = ""
+  ingress {
+    external_enabled = var.is_external_ingress
+    target_port = var.target_port_ingress
+    traffic_weight {
+      percentage = 100
+      latest_revision = true
+    }
+  }
+
+  dynamic "secret" {
+    for_each = var.secrets
+    content {
+      name = secret.key
+      key_vault_secret_id = secret.value
+    }
   }
 
   template {
@@ -14,12 +30,19 @@ resource "azurerm_container_app" "go-services" {
       name = var.name
       cpu = "0.5"
       memory = "1Gi"
-      image = "${var.acr_login_server}/${var.name}:${var.image_tag}"
+      image = "${var.acr_login_server}/${locals.image_name}:${var.image_tag}"
       dynamic "env" {
         for_each = var.envs
         content {
           name = env.key
           value = env.value
+        }
+      }
+      dynamic "env" {
+        for_each = var.secrets
+        content {
+          name = env.key
+          secret_name = env.key
         }
       }
     }
